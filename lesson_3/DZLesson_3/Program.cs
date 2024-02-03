66b38be1486af51b08642021648770e5a2b8e947
Ivan.K <kamelll2@mail.ru>
2024-02-03 16:49:51 +0500

    Создание папки lesson3

---
+++ lesson_3/DZLesson_3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine("Привет! Как я могу к тебе обращаться?");
+string MyName =  Console.ReadLine();
+Console.WriteLine(" Сколько тебе лет?");
+int MyAge = int.Parse(Console.ReadLine());
+Console.WriteLine(" а когда ты родился?");
+string birthday = Console.ReadLine();
+Console.WriteLine(" в каком городе сейчас проживаешь?");
+string city = Console.ReadLine();
+
+Console.WriteLine("Приятнол познакомиться {0}, из {1}, родившийся {2} и достигший {3} полных лет", MyName, city, birthday, MyAge);
+Console.ReadKey();
